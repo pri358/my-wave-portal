b71e3182fa87ea8f452a75212e275197aea1294d
Priyanshi Jain <priyanshi.jain030@gmail.com>
2021-12-04 21:38:01 +0530

    added comments to run.js

--- scripts/run.js
+++ scripts/run.js
@@ -1,9 +1,11 @@
 const { hexStripZeros } = require("ethers/lib/utils")
 
 const main = async () => {
-    const waveContractFactory = await hre.ethers.getcontractFactory('WavePortal');
+    // compile our contract and generate the necessary files we need to work with our contract under the artifacts
+    const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+    //Hardhat will create a local Ethereum network for us, but just for this contract. Then, after the script completes it'll destroy that local network. So, every time you run the contract, it'll be a fresh blockchain
     const waveContract = await waveContractFactory.deploy();
-    await waveContract.deploy();
+    await waveContract.deployed();
     console.log("Contract deployed to:", waveContract.address);
 };
 
