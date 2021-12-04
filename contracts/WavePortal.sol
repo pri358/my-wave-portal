f955f84505d0948e9ced75083fb60eee7d53665b
Priyanshi Jain <priyanshi.jain030@gmail.com>
2021-12-04 21:52:34 +0530

    add code to self-wave

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -1,11 +1,29 @@
 // SPDX-License-Identifier: UNLICENSED
 
+// pragma tells the compiler which solidity version to use 
 pragma solidity ^0.8.4;
 
+// for console logs given by HardHat
 import "hardhat/console.sol";
 
+// just like classes in java 
 contract WavePortal {
+    //state variable: permanently stored in contact storage
+    uint256 totalWaves;
+
     constructor() {
         console.log("Hey, I'm new here!!");
+    }
+
+    // public keyword makes function publically available on the blockchain
+    function wave() public {
+        totalWaves += 1;
+        // msg.sender: wallet address of the person who called the function
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,12 +1,26 @@
 const { hexStripZeros } = require("ethers/lib/utils")
 
 const main = async () => {
+    // get random wallet addresses from hardHat
+    const [owner, randomPerson] = await hre.ethers.getSigners();
+    
     // compile our contract and generate the necessary files we need to work with our contract under the artifacts
     const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+    
     //Hardhat will create a local Ethereum network for us, but just for this contract. Then, after the script completes it'll destroy that local network. So, every time you run the contract, it'll be a fresh blockchain
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
+
     console.log("Contract deployed to:", waveContract.address);
+    console.log("Contract deployed by:", owner.address);
+
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+
+    let waveTxn = await waveContract.wave();
+    await waveTxn.wait()
+
+    waveCount = await waveContract.getTotalWaves();
 };
 
 const runMain = async () => {
